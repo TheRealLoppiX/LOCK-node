0ca3cce3484e546205856706fe9a1b9e74c54279
TheRealLoppiX <tutuzao2016@gmail.com>
2025-08-29 14:31:31 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import fastify from "fastify";
+import { supabase } from "./supabaseConnection";
+import crypto = require("crypto");
+
+const app = fastify();
+
+type Users = {
+    name: string
+    email: string
+};
+
+
+app.get("/users", async () => {
+
+    try {
+        const { data: users } = await supabase.from("users").select("*");
+
+        return { value: users };
+    } catch(error) {
+        console.error(error);
+        throw error;
+    }
+});
+
+app.post("/users", async (request, response) => {
+
+    try {
+        const { name, email } = request.body as Users
+
+        const { data: createdUser } = await supabase.from("users").insert([{
+            name,
+            email
+        }]);
+
+    return {
+        value: createdUser ? createdUser[0] : null
+    }
+    } catch(error) {
+        console.error(error);
+        throw error;
+    };
+});
+
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then(() => {
+    console.log('Servidor funcionando')
+});
